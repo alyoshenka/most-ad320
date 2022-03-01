fd9d14ec6e0756cbc2715c4dd546b9d7c07b1a0f
Alexi <alexibmost@gmail.com>
2022-03-01 11:10:37 -0800

    nuke everything. start from same place as panzer-ad320

--- service/app.js
+++ service/app.js
@@ -1,35 +1,69 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import { body } from 'express-validator'
 
-import { router } from './routes/router.js'
+import {
+  deckById,
+  getDecks,
+  createDeck,
+  createCard,
+  deleteDeck,
+  updateDeck
+} from './handlers/decks.js'
+
+import {
+  getUsers
+} from './handlers/users.js'
 
 const app = express()
 const port = 8000
 
-const connectionString = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@notablecluster.sm4hi.mongodb.net/Notable?retryWrites=true&w=majority`
+// Connect to MongoDB
 
+const connectionString = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@crit-cluster.bpw1p.mongodb.net/notoriety?retryWrites=true&w=majority`
 try {
-    await mongoose.connect(connectionString)
+  await mongoose.connect(connectionString)
 } catch (err) {
-    console.log('error ', err)
+  console.log('error ', err)
 }
 
-const exampleMiddleware = (req, res, next) => {
-    console.log('example middleware')
-    next()
-}
+// Middleware
 
 app.use(cors())
 app.use(express.json())
 
-app.use('/', router)
+// Routes
 
-app.get('/', (req, res) => {
-    res.send('hello world')
-})
+const notImplemented = (req, res) => {
+  res.status(503).send(`Route not defined for ${req.url}`)
+}
+
+// Deck Routes
+app.get('/decks', getDecks)
+app.get('/decks/:id', deckById)
+app.post('/decks', body('name').not().isEmpty(), createDeck)
+app.put(
+  '/decks/:id',
+  body('name').not().isEmpty(),
+  updateDeck
+)
+app.delete('/decks/:id', deleteDeck)
+
+app.post(
+  '/decks/:id/cards',
+  body('frontImage').isURL(),
+  body('frontText').not().isEmpty(),
+  createCard
+)
+
+// User Routes
+app.get('/users', getUsers)
+app.get('/users/:id', notImplemented)
+app.post('/users', notImplemented)
+app.put('/users/:id', notImplemented)
+app.delete('/users/:id', notImplemented)
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-    console.log(mongoose.connection.readyState === 1 ? "mongoose ready" : "mongoose not ready")
+  console.log(`Example app listening on port ${port}!`)
 })
